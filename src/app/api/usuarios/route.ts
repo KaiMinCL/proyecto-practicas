df790f372881cbf69ba442163d7847e87f80b312
KaiMinCL <david.alvarez485@.com>
2025-05-25 21:46:04 -0400

    fix(api): Eliminación import no usado

--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { verifyUserSession } from '@/lib/auth';
-import type { NextRequest } from 'next/server';
 
 export async function GET() {
   try {
