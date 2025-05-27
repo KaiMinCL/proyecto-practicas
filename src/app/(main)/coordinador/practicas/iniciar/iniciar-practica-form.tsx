62d3c31df8c4097ab280ba9d96e427f0ac80bf21
KaiMinCL <david.alvarez485@.com>
2025-05-26 23:31:33 -0400

    fix: eliminación import sin usar de iniciar-practica-form

--- src/app/(main)/coordinador/practicas/iniciar/iniciar-practica-form.tsx
+++ src/app/(main)/coordinador/practicas/iniciar/iniciar-practica-form.tsx
@@ -44,7 +44,7 @@ import {
     SelectTrigger,
     SelectValue 
 } from "@/components/ui/select";
-import { Check, ChevronsUpDown, Save, CalendarIcon, XCircle } from "lucide-react";
+import { Check, ChevronsUpDown, Save, CalendarIcon} from "lucide-react";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import { cn } from "@/lib/utils";
